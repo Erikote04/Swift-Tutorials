561fc55e088862d765892cd6680062ce52d17ec8
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 13:08:56 +0200

    [SwiftUI Foundations](Layout and style): Creating the welcome page

---
+++ OnboardingFlow/OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Erik Sebastian de Erice Jerez on 27/5/25.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text("Welcome to My App")
+            .font(.title)
+            .fontWeight(.semibold)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}

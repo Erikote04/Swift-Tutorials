622b7c97e17cdc571d98fb5c23c2eb1d1b8e1fe7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 11:03:04 +0200

    [SwiftUI Foundations](Explore Xcode): Wrap up. Extending the app

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 24) {
             Text("Knock, knock!")
                 .padding()
                 .background(.yellow, in: .rect(cornerRadius: 8))
+                .shadow(radius: 8)
             
             Text("Who's there?")
                 .padding()
                 .background(.teal, in: .rect(cornerRadius: 8))
+                .shadow(radius: 8)
+            
+            Text("Joe")
+                .padding()
+                .background(.yellow, in: .rect(cornerRadius: 8))
+                .shadow(radius: 8)
+            
+            Text("Joe who?")
+                .padding()
+                .background(.teal, in: .rect(cornerRadius: 8))
+                .shadow(radius: 8)
+            
+            Text("Joe mama")
+                .padding()
+                .background(.yellow, in: .rect(cornerRadius: 8))
+                .shadow(radius: 8)
         }
         .padding()
     }

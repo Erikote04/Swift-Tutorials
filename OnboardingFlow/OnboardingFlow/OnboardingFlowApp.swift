89e9fcf8959df0a0cd9add2a8b48f7e09bd947d1
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 12:16:17 +0200

    [SwiftUI Foundations](Layout and style): Init project

---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Erik Sebastian de Erice Jerez on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

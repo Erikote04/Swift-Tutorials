5e75182a92aa6acd58c99da58850ce511958579a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 20:51:58 +0200

    [SwiftUI Foundations](List and text fields): Wrap up. Extending the app

--- Pick-a-Pal/Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/Pick-a-Pal/ContentView.swift
@@ -12,67 +12,86 @@ struct ContentView: View {
     @State private var nameToAdd: String = ""
     @State private var pickedName: String = ""
     @State private var shouldRemovePickedName: Bool = false
+    @State private var savedNames: [String] = []
     
     var body: some View {
-        VStack {
-            VStack(spacing: 8) {
-                Image(systemName: "person.3.sequence.fill")
-                    .foregroundStyle(.tint)
-                    .symbolRenderingMode(.hierarchical)
-                
-                Text("Pick-a-Pal")
-            }
-            .font(.title.bold())
-            
-            Text(pickedName.isEmpty ? " " : pickedName)
-                .font(.title2.bold())
-                .foregroundStyle(.tint)
-            
-            List {
-                ForEach(names, id: \.description) { name in
-                    Text(name)
+        NavigationStack {
+            VStack {
+                VStack(spacing: 8) {
+                    Image(systemName: "person.3.sequence.fill")
+                        .foregroundStyle(.tint)
+                        .symbolRenderingMode(.hierarchical)
+                    
+                    Text("Pick-a-Pal")
                 }
-            }
-            .listStyle(.plain)
-            
-            TextField("Add name", text: $nameToAdd)
-                .padding(.vertical)
-                .autocorrectionDisabled()
-                .onSubmit {
-                    if !nameToAdd.isEmpty {
-                        names.append(nameToAdd)
+                .font(.title.bold())
+                
+                Text(pickedName.isEmpty ? " " : pickedName)
+                    .font(.title2.bold())
+                    .foregroundStyle(.tint)
+                
+                List {
+                    ForEach(names, id: \.description) { name in
+                        Text(name)
+                    }
+                }
+                .listStyle(.plain)
+                
+                TextField("Add name", text: $nameToAdd)
+                    .padding(.vertical)
+                    .autocorrectionDisabled()
+                    .onSubmit {
+                        let trimmedName = nameToAdd.trimmingCharacters(in: .whitespacesAndNewlines)
+                        
+                        if !trimmedName.isEmpty && !names.contains(trimmedName) {
+                            names.append(trimmedName)
+                        }
+                        
                         nameToAdd = ""
                     }
-                }
-            
-            Divider()
-            
-            Toggle("Remove when picked", isOn: $shouldRemovePickedName)
-                .padding(.vertical)
-                .tint(.accentColor)
-            
-            Divider()
-            
-            Button {
-                if let randomName = names.randomElement() {
-                    pickedName = randomName
-                    
-                    if shouldRemovePickedName {
-                        names.removeAll { $0 == pickedName }
+                
+                Divider()
+                
+                Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+                    .padding(.vertical)
+                    .tint(.accentColor)
+                
+                Divider()
+                
+                Button {
+                    if let randomName = names.randomElement() {
+                        pickedName = randomName
+                        
+                        if shouldRemovePickedName {
+                            names.removeAll { $0 == pickedName }
+                        }
+                    } else {
+                        pickedName = ""
                     }
-                } else {
-                    pickedName = ""
+                } label: {
+                    Text("Pick Random Name")
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 16)
                 }
-            } label: {
-                Text("Pick Random Name")
-                    .padding(.vertical, 8)
-                    .padding(.horizontal, 16)
+                .padding(.vertical)
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
             }
-            .padding(.vertical)
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Save") {
+                        savedNames = names
+                    }
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Load") {
+                        names = savedNames
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 

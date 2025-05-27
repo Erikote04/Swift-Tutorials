7460b4f2fdac24b3c4a3ccd0d4a9fefde06d53a2
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 10:48:46 +0200

    [SwiftUI Foundations](Explore Xcode): Init project

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Erik Sebastian de Erice Jerez on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

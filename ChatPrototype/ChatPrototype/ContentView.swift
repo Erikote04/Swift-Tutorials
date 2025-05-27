3ce8bd4181d82d8a25f1edaadced83cb5dd5efcf
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 10:54:42 +0200

    [SwiftUI Foundations](Explore Xcode): Adding modifiers

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -11,7 +11,12 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Knock, knock!")
+                .padding()
+                .background(.yellow, in: .rect(cornerRadius: 8))
+            
             Text("Who's there?")
+                .padding()
+                .background(.teal, in: .rect(cornerRadius: 8))
         }
         .padding()
     }

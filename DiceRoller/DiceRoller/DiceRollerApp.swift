d74f7f4b97fe4603b35b6839e3eea4d31b542e10
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 19:21:03 +0200

    [SwiftUI Foundations](Buttons and state): Init project

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Erik Sebastian de Erice Jerez on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

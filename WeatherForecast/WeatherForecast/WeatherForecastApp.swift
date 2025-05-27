0360af12152dc583654a03a3c201a71027b37834
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 11:06:38 +0200

    [SwiftUI Foundations](Views, structures and properties): Init project

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Erik Sebastian de Erice Jerez on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

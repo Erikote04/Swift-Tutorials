4ea541b8ec6378acd80dc817c8b71af0c1d40b4e
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-27 11:20:12 +0200

    [SwiftUI Foundations](Views, structures and properties): Using computed properties for the icon and color

--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
-            DayForecast(day: "Tue", high: 60, low: 40)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
         .padding()
     }
@@ -23,15 +23,24 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let isRainy: Bool
     let high: Int
     let low: Int
+    
+    var iconName: String {
+        return isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        return isRainy ? .blue : .yellow
+    }
     
     var body: some View {
         VStack {
             Text(day)
             
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
             
             Text("High: \(high)º")
             

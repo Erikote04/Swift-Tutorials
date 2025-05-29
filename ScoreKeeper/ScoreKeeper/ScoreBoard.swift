fa36df8716b35958d7bc1eca51d14a1322a4b4be
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-29 11:03:41 +0200

    [Data Modeling](Custom types and Swift Testing): Creating the scoreboard

---
+++ ScoreKeeper/ScoreKeeper/ScoreBoard.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreBoard.swift
+//  ScoreKeeper
+//
+//  Created by Erik Sebastian de Erice Jerez on 29/5/25.
+//
+
+import Foundation
+
+struct ScoreBoard {
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
+}

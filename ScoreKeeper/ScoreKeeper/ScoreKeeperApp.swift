ec4babf89ecdb1f9e1f7fd68b87ee310ecacda0a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-29 10:36:22 +0200

    [Data Modeling](Custom types and Swift Testing): Init project

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Erik Sebastian de Erice Jerez on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

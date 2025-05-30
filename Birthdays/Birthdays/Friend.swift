c8da8c5ff828c4bddfd7f1bd1534c29ad41eef4d
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-30 12:30:06 +0200

    [Data Modeling](Models and persistence): Creating a type to represent a friend

---
+++ Birthdays/Birthdays/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Erik Sebastian de Erice Jerez on 30/5/25.
+//
+
+import Foundation
+
+struct Friend {
+    var name: String
+    var birthday: Date
+}

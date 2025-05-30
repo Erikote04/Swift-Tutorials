554a8fa0d1667bbeabeb05ff659f505f7c32ca13
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-30 13:19:41 +0200

    [Data Modeling](Navigation, editing, and relationships): Init project

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Erik Sebastian de Erice Jerez on 30/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

dff83243054622456ea7472698ceb8384516db26
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-05-30 12:26:39 +0200

    [Data Modeling](Models and persistence): Init project

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Erik Sebastian de Erice Jerez on 30/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

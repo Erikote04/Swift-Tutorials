60c79dee1ee7e75882689239f6409a46b77e6f24
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-02 14:20:04 +0200

    [Data Modeling](Observation and shareable data models): Connecting views to Alphabetizer

---
+++ Alphabetizer/Alphabetizer/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by Erik Sebastian de Erice Jerez on 2/6/25.
+//
+
+import Foundation
+
+enum Message: String {
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}

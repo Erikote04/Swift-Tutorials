ad650a878b199c5a4a38b342807da59edc1236e3
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-02 16:48:05 +0200

    [Machine Learning](Natural language): Init project

---
+++ HikingSurvey/HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Erik Sebastian de Erice Jerez on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

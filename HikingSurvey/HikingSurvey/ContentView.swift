14f895f1a5c0de909ab2615c1355614ceb4a9c52
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-05 14:14:21 +0200

    [Machine Learning](Natural language): Modeling survey responses

--- HikingSurvey/HikingSurvey/ContentView.swift
+++ HikingSurvey/HikingSurvey/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var responses: [Response] = []
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Opinions on Hiking")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .padding(.top, 24)
+            
+            ScrollView {
+                ForEach(responses) { response in
+                    Text(response.text)
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            for response in Response.sampleResponses {
+                responses.insert(Response(text: response), at: 0)
+            }
+        }
+        .padding(.horizontal)
+        .background(Color(white: 0.95))
     }
 }
 

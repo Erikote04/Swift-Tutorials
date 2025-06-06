c579bf2deb31b149fd7113c8121e6807a4495041
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-06 13:54:56 +0200

    [Machine Learning](Recognize text in images): Init project

---
+++ SignDecoder/SignDecoder/SignDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignDecoderApp.swift
+//  SignDecoder
+//
+//  Created by Erik Sebastian de Erice Jerez on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SignDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

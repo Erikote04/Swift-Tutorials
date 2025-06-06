08128dee461df107f7797169c73844dc33b7f588
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-06 20:26:00 +0200

    [Machine Learning](Custom models with Core ML): Creating a Risk model

---
+++ TrailAnalyzer/TrailAnalyzer/Views/RiskCard.swift
@@ -0,0 +1,34 @@
+//
+//  RiskCard.swift
+//  TrailAnalyzer
+//
+//  Created by Erik Sebastian de Erice Jerez on 6/6/25.
+//
+
+import SwiftUI
+
+struct RiskCard: View {
+    @State var risk: Risk
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(risk.image)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                
+                Text(risk.rawValue)
+                    .font(.title2)
+            }
+            
+            Text(risk.description)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(16)
+        .background(.white, in: .rect(cornerRadius: 12))
+    }
+}
+
+#Preview {
+    RiskCard(risk: .moderate)
+}

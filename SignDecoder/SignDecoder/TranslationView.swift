6546738b28315edb2c9129d339496a5378e5a65f
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-06 14:02:13 +0200

    [Machine Learning](Recognize text in images): Translating text to another language

---
+++ SignDecoder/SignDecoder/TranslationView.swift
@@ -0,0 +1,41 @@
+//
+//  TranslationView.swift
+//  SignDecoder
+//
+//  Created by Erik Sebastian de Erice Jerez on 6/6/25.
+//
+
+import SwiftUI
+import Translation
+
+struct TranslationView: View {
+    var text: String
+    @State private var showingTranslation = false
+    
+    var body: some View {
+        VStack {
+            Text("Identified Text")
+                .font(.subheadline.bold())
+                .textCase(.uppercase)
+                .foregroundStyle(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+            
+            Text(text)
+                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .topLeading)
+                .padding()
+                .background(Color(white: 0.9))
+                .translationPresentation(isPresented: $showingTranslation, text: text)
+            
+            Button {
+                showingTranslation = true
+            } label: {
+                Text("Translate")
+            }
+        }
+    }
+}
+
+#Preview {
+    TranslationView(text: "Caution, falling rocks")
+}

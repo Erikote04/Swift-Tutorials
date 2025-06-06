890a3fbb26327442a56eaae6d99321023016970a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-06 20:14:59 +0200

    [Machine Learning](Custom models with Core ML): Init project

---
+++ TrailAnalyzer/TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by Erik Sebastian de Erice Jerez on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

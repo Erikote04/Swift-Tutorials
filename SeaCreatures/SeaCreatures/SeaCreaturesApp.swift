85f22049276b4a1e99dff0735619d7feff6b28b5
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-09 10:49:53 +0200

    [Spatial Computing](Volumes in visionOS): Init project

---
+++ SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Erik Sebastian de Erice Jerez on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

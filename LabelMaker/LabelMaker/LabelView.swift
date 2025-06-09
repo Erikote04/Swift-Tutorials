fc08f89e66177d795de5eeae7247b2ee0f98f9df
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-09 10:44:47 +0200

    [Spatial Computing](Ornaments and multiple windows): Improving the app design

--- LabelMaker/LabelMaker/LabelView.swift
+++ LabelMaker/LabelMaker/LabelView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct LabelView: View {
+    @Environment(\.isEnabled) private var isEnabled
     @Binding var label: Label
     
     var body: some View {
         TextField("Type to enter text", text: $label.text, axis: .vertical)
-            .frame(width: 500, height: 500)
-            .padding()
+            .frame(width: 500, height: isEnabled ? 500 : nil)
+            .padding(50)
             .background(label.selectedColor(), in: .rect(cornerRadius: label.cornerRadius))
             .foregroundStyle(.black)
             .font(.system(size: 40, weight: .semibold))
@@ -22,6 +23,7 @@ struct LabelView: View {
 }
 
 #Preview {
-    @Previewable @State var label = Label()
+    @Previewable @State var label = Label(text: "The label's text content")
     LabelView(label: $label)
+        .disabled(true)
 }

76b06b1136c71619c56b8b1abd0f6eb56689811f
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-09 09:58:30 +0200

    [Spatial Computing](Ornaments and multiple windows): Init project

---
+++ LabelMaker/LabelMaker/LabelMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelMakerApp.swift
+//  LabelMaker
+//
+//  Created by Erik Sebastian de Erice Jerez on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LabelMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

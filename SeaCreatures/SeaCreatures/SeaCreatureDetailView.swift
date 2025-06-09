69b7e0b7378ea8dcf540fa58af4ee6ea13f482fe
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-09 14:27:46 +0200

    [Spatial Computing](Volumes in visionOS): Dragging the model to rotate

--- SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
+++ SeaCreatures/SeaCreatures/SeaCreatureDetailView.swift
@@ -12,8 +12,26 @@ import RealityKitContent
 struct SeaCreatureDetailView: View {
     let modelName: String
     
+    @State private var verticalRotation: CGFloat = .zero
+    @State private var horizontalRotation: CGFloat = .zero
+    @State private var endVerticalRotation: CGFloat = .zero
+    @State private var endHorizontalRotation: CGFloat = .zero
+    
     var body: some View {
         Model3D(named: modelName, bundle: realityKitContentBundle)
+            .rotation3DEffect(.degrees(verticalRotation), axis: .x)
+            .rotation3DEffect(.degrees(horizontalRotation), axis: .y)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        verticalRotation = value.translation.height + endVerticalRotation
+                        horizontalRotation = value.translation.width + endHorizontalRotation
+                    }
+                    .onEnded { value in
+                        endVerticalRotation = verticalRotation
+                        endHorizontalRotation = horizontalRotation
+                    }
+            )
     }
 }
 

d58d0993de05250bb3bac2a78892fbe8e4f526c7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-09 09:09:44 +0200

    [Special Computing](Windows in visionOS): Init project

---
+++ TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Erik Sebastian de Erice Jerez on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

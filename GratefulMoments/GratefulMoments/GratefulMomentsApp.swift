f66ac9574f9ffc30f66b74b4a364e1fbcef11ee3
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-11-16 19:31:08 +0100

    [App Development](Views and data storage): Creating a data container

--- GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -5,13 +5,18 @@
 //  Created by Erik Sebastian de Erice Jerez on 16/11/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct GratefulMomentsApp: App {
+    let dataContainer = DataContainer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContainer)
         }
+        .modelContainer(dataContainer.modelContainer)
     }
 }

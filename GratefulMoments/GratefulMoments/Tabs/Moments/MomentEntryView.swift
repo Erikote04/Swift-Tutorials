b4ab64769b19730dba4a5300fe53d646131446b8
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-11-16 20:24:33 +0100

    [App Development](Views and data storage): Adding, saving and canceling features

--- GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -6,13 +6,18 @@
 //
 
 import PhotosUI
+import SwiftData
 import SwiftUI
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
-    @State private var image: PhotosPickerItem?
     @State private var imageData: Data?
+    @State private var image: PhotosPickerItem?
+    @State private var isShowingCancelConfirmation = false
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(DataContainer.self) private var dataContainer
     
     var body: some View {
         NavigationStack {
@@ -21,6 +26,43 @@ struct MomentEntryView: View {
             }
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful For")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("cancel", systemImage: "xmark") {
+                        if title.isEmpty, note.isEmpty, imageData == nil {
+                            dismiss()
+                        } else {
+                            isShowingCancelConfirmation = true
+                        }
+                    }
+                    .confirmationDialog("Discard Moment", isPresented: $isShowingCancelConfirmation) {
+                        Button("Discard Moment", role: .destructive) {
+                            dismiss()
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add", systemImage: "checkmark") {
+                        let moment = Moment(
+                            title: title,
+                            note: note,
+                            imageData: imageData,
+                            timestamp: .now
+                        )
+                        
+                        dataContainer.context.insert(moment)
+                        
+                        do {
+                            try dataContainer.context.save()
+                            dismiss()
+                        } catch {
+                            // Don't dismiss
+                        }
+                    }
+                    .disabled(title.isEmpty)
+                }
+            }
         }
     }
     
@@ -72,4 +114,5 @@ struct MomentEntryView: View {
 
 #Preview {
     MomentEntryView()
+        .sampleDataContainer()
 }

5f9cbeb0763f314f6e524b0bbed92ba24913c940
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-11-16 19:16:48 +0100

    [App Development](Views and data storage): Adding a photo picker

--- GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -5,11 +5,14 @@
 //  Created by Erik Sebastian de Erice Jerez on 16/11/25.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
+    @State private var image: PhotosPickerItem?
+    @State private var imageData: Data?
     
     var body: some View {
         NavigationStack {
@@ -34,8 +37,36 @@ struct MomentEntryView: View {
             TextField("Log your small wins", text: $note, axis: .vertical)
                 .multilineTextAlignment(.leading)
                 .lineLimit(5...Int.max)
+            
+            photoPicker
         }
         .padding()
+    }
+    
+    private var photoPicker: some View {
+        PhotosPicker(selection: $image) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: image) {
+            guard let image else { return }
+            
+            Task {
+                imageData = try await image.loadTransferable(type: Data.self)
+            }
+        }
     }
 }
 

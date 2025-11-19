63104dc9be233aa1feb65d4bae96283656fec16f
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-11-19 14:21:43 +0100

    [App Development](User experience features): Calculating and showing streaks

---
+++ GratefulMoments/GratefulMoments/Tabs/Achievements/StreakView.swift
@@ -0,0 +1,31 @@
+//
+//  StreakView.swift
+//  GratefulMoments
+//
+//  Created by Erik Sebastian de Erice Jerez on 19/11/25.
+//
+
+import SwiftUI
+
+struct StreakView: View {
+    var numberOfDays: Int
+    
+    var body: some View {
+        Hexagon(borderColor: .secondary) {
+            VStack(spacing: .zero) {
+                Text("Streak \(Image(systemName: "flame.fill"))")
+                    .foregroundStyle(.ember)
+                
+                Text("\(numberOfDays)")
+                    .font(.system(size: 70))
+                
+                Text("Days")
+            }
+            .font(.callout)
+        }
+    }
+}
+
+#Preview {
+    StreakView(numberOfDays: 23)
+}
